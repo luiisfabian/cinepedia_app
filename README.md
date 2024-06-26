14859040c1ea41aaa86b0d53c2b48935e42a5b95
FabianM <luiisfabian96@gmail.com>
2024-06-26 18:44:03 -0500

    iniciaizacion del proyecyto, creacion del goRouter, y el themeinicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# cinemapedia_app
+
+A new Flutter project.
---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  static const name = "homeScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: const Placeholder(),
+    );
+  }
+}
