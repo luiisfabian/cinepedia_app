6fb00c2445ed2516c2fc8c6a2bed696783b21bfe
FabianM <luiisfabian96@gmail.com>
2024-06-28 01:04:58 -0500

    Creacion del datasource de infraestructure para comunicacion con el api

---
+++ lib/infrastructure/movieDb_datasource.dart
@@ -0,0 +1,21 @@
+import 'package:cinemapedia_app/config/constants/environment.dart';
+import 'package:cinemapedia_app/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia_app/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
+
+class MovieDbDatasource extends MoviesDatasource {
+  final dio = Dio(BaseOptions(
+      baseUrl: "https://api.themoviedb.org/3/",
+      queryParameters: {
+        'api_key': Environment.movieDbKey,
+        'lenguaje': 'es-MX'
+      }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('movies/now-playing');
+    final List<Movie> movies = [];
+
+    return movies;
+  }
+}
