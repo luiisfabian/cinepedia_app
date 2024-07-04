d2f7d1e15d44eedd2f8625f7ce14edbc9bc447ea
FabianM <luiisfabian96@gmail.com>
2024-07-04 09:45:47 -0500

    Implementacion del repository

---
+++ lib/infrastructure/repositories/movie_repository_imple.dart
@@ -0,0 +1,14 @@
+import 'package:cinemapedia_app/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia_app/domain/entities/movie.dart';
+import 'package:cinemapedia_app/domain/repositories/movies_repository.dart';
+
+class MovieRespositoryImplementation extends MoviesRepository {
+  final MoviesDatasource datasource;
+
+  MovieRespositoryImplementation({required this.datasource});
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return this.datasource.getNowPlaying(page: page);
+  }
+}
