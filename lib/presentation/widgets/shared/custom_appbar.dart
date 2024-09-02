f0c5ccbdc05d948fbac1f49e6ad84f3c6b367386
FabianM <luiisfabian96@gmail.com>
2024-09-02 09:50:56 -0500

    Accion de ir a la pelicula desde el buscador

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,7 +1,9 @@
+import 'package:cinemapedia_app/domain/entities/movie.dart';
 import 'package:cinemapedia_app/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinemapedia_app/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
@@ -30,12 +32,20 @@ class CustomAppbar extends ConsumerWidget {
                   style: titleStyle,
                 ),
                 const Spacer(),
-                IconButton(onPressed: () {
+                IconButton(
+                    onPressed: () async {
+                      final movieRepository = ref.read(movieRepositoryProvider);
 
-                  final movieRepository = ref.read(movieRepositoryProvider);
-                    
-                  showSearch(context: context, delegate: SearchMovieDelegate(searchMovies: movieRepository.searchMovies));
-                }, icon: const Icon(Icons.search))
+                      showSearch<Movie?>(
+                        context: context,
+                        delegate: SearchMovieDelegate(
+                            searchMovies: movieRepository.searchMovies),
+                      ).then((movie) {
+                        if (movie == null) return;
+                        context.push('/movie/${movie.id}');
+                      });
+                    },
+                    icon: const Icon(Icons.search))
               ],
             ),
           ),
