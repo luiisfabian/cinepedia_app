9c8a4c5bc17f1c3bdea92da1d768b30d2dc19ac0
FabianM <luiisfabian96@gmail.com>
2024-09-06 08:19:36 -0500

    mantener el estado de la busqueda de la pelicula

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,6 +1,7 @@
 import 'package:cinemapedia_app/domain/entities/movie.dart';
 import 'package:cinemapedia_app/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinemapedia_app/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:cinemapedia_app/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -13,42 +14,54 @@ class CustomAppbar extends ConsumerWidget {
     final colors = Theme.of(context);
     final titleStyle = Theme.of(context).textTheme.titleMedium;
     return SafeArea(
-        bottom: false,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: SizedBox(
-            width: double.infinity,
-            child: Row(
-              children: [
-                Icon(
-                  Icons.movie_outlined,
-                  color: colors.primaryColor,
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  "Cinemapedia",
-                  style: titleStyle,
-                ),
-                const Spacer(),
-                IconButton(
-                    onPressed: () async {
-                      final movieRepository = ref.read(movieRepositoryProvider);
+      bottom: false,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            children: [
+              Icon(
+                Icons.movie_outlined,
+                color: colors.primaryColor,
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Text(
+                "Cinemapedia",
+                style: titleStyle,
+              ),
+              const Spacer(),
+              IconButton(
+                onPressed: () async {
+                  final movieRepository = ref.read(movieRepositoryProvider);
+                  final searchQuery = ref.read(searchQueryProvider);
 
-                      showSearch<Movie?>(
-                        context: context,
-                        delegate: SearchMovieDelegate(
-                            searchMovies: movieRepository.searchMovies),
-                      ).then((movie) {
-                        if (movie == null) return;
-                        context.push('/movie/${movie.id}');
-                      });
+                  showSearch<Movie?>(
+                    query: searchQuery,
+                    context: context,
+                    delegate: SearchMovieDelegate(
+                      searchMovies: (query) {
+                        ref
+                            .read(searchQueryProvider.notifier)
+                            .update((state) => query);
+                        return movieRepository.searchMovies(query);
+                      },
+                    ),
+                  ).then(
+                    (movie) {
+                      if (movie == null) return;
+                      context.push('/movie/${movie.id}');
                     },
-                    icon: const Icon(Icons.search))
-              ],
-            ),
+                  );
+                },
+                icon: const Icon(Icons.search),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
